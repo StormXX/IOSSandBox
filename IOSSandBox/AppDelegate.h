c2731c0b6ccddd31f19587661ca00903d1019783
StormXX <liaozhaoxing@gmail.com>
2014-05-21 21:29:19 +0800

    Initial Commit

---
+++ IOSSandBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSSandBox
+//
+//  Created by Gintama on 14-5-21.
+//  Copyright (c) 2014年 Gintama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSSandBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOSSandBox
+//
+//  Created by Gintama on 14-5-21.
+//  Copyright (c) 2014年 Gintama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
